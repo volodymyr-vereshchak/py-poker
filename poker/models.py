fabd79b6197d1507518aa4f06c3dcd99725ceb35
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-11-13 13:56:35 +0200

    start user app

--- poker/models.py
+++ poker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Table(models.Model):
+    name = models.CharField(max_length=64)
+    buy_in = models.PositiveIntegerField()
+    chips = models.PositiveIntegerField()
+
+
+
+class GameSession(models.Model):
+    table = models.ForeignKey()
